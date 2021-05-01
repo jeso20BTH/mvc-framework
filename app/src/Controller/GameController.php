40ea4bd7a4760ea8e1332f2141e60e60d7be6eae
jeso20BTH <jeso20@student.bth.se>
2021-05-01 14:50:29 +0100

    Rewrote dupplicated code for no repeating

--- app/src/Controller/GameController.php
+++ app/src/Controller/GameController.php
@@ -18,14 +18,32 @@ class GameController extends AbstractController
         $this->session = $session;
     }
 
+    public function dataToRender(array $data)
+    {
+        return [
+            'title' => $data["title"] ?? null,
+            'header' => $data["header"] ?? null,
+            'playerVictories' => $data["standings"]["player"] ?? null,
+            'computerVictories' => $data["standings"]["computer"] ?? null,
+            'type' => $data["type"] ?? null,
+            'playerSum' => $data["playerSum"] ?? 0,
+            'computerSum' => $data["computerSum"] ?? 0,
+            'lastRoll' => $data["lastRoll"] ?? null,
+            'roller' => $data["roller"] ?? null,
+            'playerMoney' => $data["playerMoney"] ?? 0,
+            'computerMoney' => $data["computerMoney"] ?? 0,
+            'currentBet' => $data["currentBet"] ?? 0,
+            'message' => $data["message"] ?? null,
+            'graphic' => $data["graphic"] ?? null
+        ];
+    }
+
     /**
      * @Route("/", name="index")
     */
     public function greeting(): Response
     {
         $this->session->clear();
-        var_dump($this->session->get("Yatzy"));
-        var_dump($this->session->get("TwentyOne"));
 
         return $this->render('standard.html.twig', [
             'header' => "Welcome!",
@@ -44,29 +62,15 @@ class GameController extends AbstractController
     public function twentyOne(): Response
     {
         // var_dump($this->session);
-        var_dump($this->session->get("TwentyOne"));
         $callable = $this->session->get("TwentyOne") ?? new TwentyOneGame();
 
 
-        $data = $callable->renderGame();
+        $res = $callable->renderGame();
         $this->session->set("TwentyOne", $callable);
 
-        return $this->render('twentyone.html.twig', [
-            'title' => $data["title"] ?? null,
-            'header' => $data["header"] ?? null,
-            'playerVictories' => $data["standings"]["player"] ?? null,
-            'computerVictories' => $data["standings"]["computer"] ?? null,
-            'type' => $data["type"] ?? null,
-            'playerSum' => $data["playerSum"] ?? 0,
-            'computerSum' => $data["computerSum"] ?? 0,
-            'lastRoll' => $data["lastRoll"] ?? null,
-            'roller' => $data["roller"] ?? null,
-            'playerMoney' => $data["playerMoney"] ?? 0,
-            'computerMoney' => $data["computerMoney"] ?? 0,
-            'currentBet' => $data["currentBet"] ?? 0,
-            'message' => $data["message"] ?? null,
-            'graphic' => $data["graphic"] ?? null
-        ]);
+        $data = $this->dataToRender($res);
+
+        return $this->render('twentyone.html.twig', $data);
     }
 
     /**
@@ -81,26 +85,13 @@ class GameController extends AbstractController
         $callable = $this->session->get("TwentyOne") ?? new TwentyOneGame();
 
 
-        $data = $callable->postController();
+        $res = $callable->postController();
 
         $this->session->set("TwentyOne", $callable);
 
-        return $this->render('twentyone.html.twig', [
-            'title' => $data["title"] ?? null,
-            'header' => $data["header"] ?? null,
-            'playerVictories' => $data["standings"]["player"] ?? null,
-            'computerVictories' => $data["standings"]["computer"] ?? null,
-            'type' => $data["type"] ?? null,
-            'playerSum' => $data["playerSum"] ?? 0,
-            'computerSum' => $data["computerSum"] ?? 0,
-            'lastRoll' => $data["lastRoll"] ?? null,
-            'roller' => $data["roller"] ?? null,
-            'playerMoney' => $data["playerMoney"] ?? 0,
-            'computerMoney' => $data["computerMoney"] ?? 0,
-            'currentBet' => $data["currentBet"] ?? 0,
-            'message' => $data["message"] ?? null,
-            'graphic' => $data["graphic"] ?? null
-        ]);
+        $data = $this->dataToRender($res);
+
+        return $this->render('twentyone.html.twig', $data);
     }
 
     /**
@@ -108,13 +99,10 @@ class GameController extends AbstractController
     */
     public function yatzy(): Response
     {
-        var_dump($this->session->get("Yatzy"));
         $callable = $this->session->get("Yatzy") ?? new YatzyGame();
 
         $data = $callable->renderGame();
         $this->session->set("Yatzy", $callable);
-
-        var_dump($data);
 
         return $this->render('yatzy.html.twig', [
             'header' => $data["header"] ?? null,
